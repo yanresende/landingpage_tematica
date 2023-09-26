3c8f668ee7e362ca49dcb612d6b77475b2c0d378
Yan Araujo <saresende555@gmail.com>
2023-09-26 15:23:04 -0300

    e

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,20 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question');
 
-    const heroSection = document.querySelector('.hero');
-    const alturaHero = heroSection.clientHeight
-
-    window.addEventListener('scroll', function () {
-        const posicaoAtual = window.scrollY;
-
-        if (posicaoAtual < alturaHero) {
-            ocultaElementosDoHeader();
-        } else {
-            exibeElementosDoHeader();
-        }
-    })
-
-
     //Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
